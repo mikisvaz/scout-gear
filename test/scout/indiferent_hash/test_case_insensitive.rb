bf1b5bef54403c8b5457a1b3d0415087c48df41f
Miguel Vazquez <mikisvaz@gmail.com>
2023-03-27 10:19:44 +0200

    Separate case-insensitive hash

---
+++ test/scout/indiferent_hash/test_case_insensitive.rb
@@ -0,0 +1,16 @@
+require File.expand_path(__FILE__).sub(%r(/test/.*), '/test/test_helper.rb')
+require File.expand_path(__FILE__).sub(%r(.*/test/), '').sub(/test_(.*)\.rb/,'\1')
+
+class TestCaseInsensitiveHash < Test::Unit::TestCase
+  def test_case_insensitive_hash
+    a = {:a => 1, "b" => 2}
+    a.extend CaseInsensitiveHash
+
+    assert_equal 1, a[:a]
+    assert_equal 1, a["A"]
+    assert_equal 1, a[:A]
+    assert_equal 2, a["B"]
+    assert_equal 2, a[:b]
+  end
+end
+
