840873c845fcf2a8df17d32f4efa39ccd9ed0280
Miguel Vazquez <mikisvaz@gmail.com>
2023-06-22 16:26:24 +0200

    Scout templates

---
+++ share/templates/workflow.rb
@@ -0,0 +1,16 @@
+require 'scout'
+
+Misc.add_libdir if __FILE__ == $0
+
+#require 'scout/sources/MODULE'
+
+module MODULE
+  extend Workflow
+
+
+end
+
+#require 'MODULE/tasks/basic.rb'
+
+#require 'scout/knowledge_base/MODULE'
+#require 'scout/entity/MODULE'
