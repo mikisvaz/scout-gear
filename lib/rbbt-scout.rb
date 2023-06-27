32410f53c9b169ddfa0c40f62add444097031b2c
Miguel Vazquez <mikisvaz@gmail.com>
2023-06-27 18:44:27 +0200

    Make Rbbt resource equal to Scout

--- lib/rbbt-scout.rb
+++ lib/rbbt-scout.rb
@@ -1,7 +1,8 @@
 $LOAD_PATH.unshift File.join(__dir__, '../modules/rbbt-util/lib')
-module Rbbt
+module Scout
   extend Resource
   self.path_maps = Path.path_maps.merge(:rbbt_lib => File.expand_path(File.join(__dir__, '../modules/rbbt-util/', '{TOPLEVEL}','{SUBPATH}')))
 end
+Rbbt = Scout
 
 Resource.set_software_env Rbbt.software
