47d8c195560f86e9d812f6732038ac6c0292db03
Miguel Vazquez <mikisvaz@gmail.com>
2024-11-04 11:33:49 +0100

    Make sure we don't log very large messages in STDERR

--- lib/scout/workflow/step/info.rb
+++ lib/scout/workflow/step/info.rb
@@ -121,6 +121,7 @@ class Step
     if message.nil?
       Log.info [Log.color(:status, status, true), Log.color(:task, task_name, true), Log.color(:path, path)] * " "
     else
+      message = Log.fingerprint(message)
       Log.info [Log.color(:status, status, true), Log.color(:task, task_name, true), message, Log.color(:path, path)] * " "
     end
   end
