7d256d62514d53888ce76e2bc61011f8d9af5fc3
Miguel Vazquez <mikisvaz@gmail.com>
2025-03-10 19:32:35 +0100

    Allow failing if missing tokyocabinet gem

--- lib/scout/persist/engine/tokyocabinet.rb
+++ lib/scout/persist/engine/tokyocabinet.rb
@@ -1,105 +1,112 @@
-require 'tokyocabinet'
+begin
+  require 'tokyocabinet'
+  continue = true
+rescue Exception
+  Log.warn "The Tokyocabinet gem could not be loaded: TSV persistence may not work"
+  continue = false
+end
 
-module ScoutCabinet
-  attr_accessor :persistence_path, :persistence_class
+if continue
+  module ScoutCabinet
+    attr_accessor :persistence_path, :persistence_class
 
-  def self.open(path, write = true, tokyocabinet_class = TokyoCabinet::HDB)
-    path = path.find if Path === path
-    if String === tokyocabinet_class && tokyocabinet_class.include?(":big")
-      big = true
-      tokyocabinet_class = tokyocabinet_class.split(":").first
-    else
-      big = false
+    def self.open(path, write = true, tokyocabinet_class = TokyoCabinet::HDB)
+      path = path.find if Path === path
+      if String === tokyocabinet_class && tokyocabinet_class.include?(":big")
+        big = true
+        tokyocabinet_class = tokyocabinet_class.split(":").first
+      else
+        big = false
+      end
+
+      dir = File.dirname(File.expand_path(path))
+      Open.mkdir(dir) unless File.exist?(dir)
+
+      tokyocabinet_class = tokyocabinet_class.to_s if Symbol === tokyocabinet_class
+      tokyocabinet_class = TokyoCabinet::HDB if tokyocabinet_class == "HDB" or tokyocabinet_class.nil?
+      tokyocabinet_class = TokyoCabinet::BDB if tokyocabinet_class == "BDB"
+
+      # Hack - Ignore warning: undefining the allocator of T_DATA class
+      # TokyoCabinet::HDB_data
+      database = Log.ignore_stderr do Persist::CONNECTIONS[path] ||= tokyocabinet_class.new end
+
+      if big and not Open.exists?(path)
+        database.tune(nil, nil, nil, tokyocabinet_class::TLARGE | tokyocabinet_class::TDEFLATE) 
+      end
+
+      flags = (write ? tokyocabinet_class::OWRITER | tokyocabinet_class::OCREAT : tokyocabinet_class::OREADER)
+      database.close 
+
+      if !database.open(path, flags)
+        ecode = database.ecode
+        raise "Open error: #{database.errmsg(ecode)}. Trying to open file #{path}"
+      end
+
+      database.extend ScoutCabinet
+      database.persistence_path ||= path
+      database.persistence_class = tokyocabinet_class
+
+      database.open(path, tokyocabinet_class::OREADER)
+
+      database.define_singleton_method(:fingerprint){ "#{self.persistence_class}:#{self.persistence_path}" }
+
+      Persist::CONNECTIONS[path] = database
+
+      database
     end
 
-    dir = File.dirname(File.expand_path(path))
-    Open.mkdir(dir) unless File.exist?(dir)
-
-    tokyocabinet_class = tokyocabinet_class.to_s if Symbol === tokyocabinet_class
-    tokyocabinet_class = TokyoCabinet::HDB if tokyocabinet_class == "HDB" or tokyocabinet_class.nil?
-    tokyocabinet_class = TokyoCabinet::BDB if tokyocabinet_class == "BDB"
-
-    # Hack - Ignore warning: undefining the allocator of T_DATA class
-    # TokyoCabinet::HDB_data
-    database = Log.ignore_stderr do Persist::CONNECTIONS[path] ||= tokyocabinet_class.new end
-
-    if big and not Open.exists?(path)
-      database.tune(nil, nil, nil, tokyocabinet_class::TLARGE | tokyocabinet_class::TDEFLATE) 
+    def close
+      @closed = true
+      @writable = false
+      super
     end
 
-    flags = (write ? tokyocabinet_class::OWRITER | tokyocabinet_class::OCREAT : tokyocabinet_class::OREADER)
-    database.close 
+    def read(force = false)
+      return if ! @writable && ! @closed && ! force
+      self.close
+      if !self.open(@persistence_path, persistence_class::OREADER)
+        ecode = self.ecode
+        raise "Open error: #{self.errmsg(ecode)}. Trying to open file #{@persistence_path}"
+      end
 
-    if !database.open(path, flags)
-      ecode = database.ecode
-      raise "Open error: #{database.errmsg(ecode)}. Trying to open file #{path}"
+      @writable = false
+      @closed = false
+
+      self
     end
 
-    database.extend ScoutCabinet
-    database.persistence_path ||= path
-    database.persistence_class = tokyocabinet_class
-
-    database.open(path, tokyocabinet_class::OREADER)
-
-    database.define_singleton_method(:fingerprint){ "#{self.persistence_class}:#{self.persistence_path}" }
-
-    Persist::CONNECTIONS[path] = database
-
-    database
-  end
-
-  def close
-    @closed = true
-    @writable = false
-    super
-  end
-
-  def read(force = false)
-    return if ! @writable && ! @closed && ! force
-    self.close
-    if !self.open(@persistence_path, persistence_class::OREADER)
-      ecode = self.ecode
-      raise "Open error: #{self.errmsg(ecode)}. Trying to open file #{@persistence_path}"
+    def write?
+      @writable
     end
 
-    @writable = false
-    @closed = false
-
-    self
-  end
-
-  def write?
-    @writable
-  end
-
-  def closed?
-    @closed
-  end
-
-
-  def write(force = true)
-    return if write? && ! closed? && ! force
-    self.close
-
-    if !self.open(@persistence_path, persistence_class::OWRITER)
-      ecode = self.ecode
-      raise "Open error: #{self.errmsg(ecode)}. Trying to open file #{@persistence_path}"
+    def closed?
+      @closed
     end
 
-    @writable = true
-    @closed = false
 
-    self
-  end
+    def write(force = true)
+      return if write? && ! closed? && ! force
+      self.close
 
-  def write_and_read
-    begin
-      write
-      yield
-    ensure
-      read
+      if !self.open(@persistence_path, persistence_class::OWRITER)
+        ecode = self.ecode
+        raise "Open error: #{self.errmsg(ecode)}. Trying to open file #{@persistence_path}"
+      end
+
+      @writable = true
+      @closed = false
+
+      self
     end
-  end
+
+    def write_and_read
+      begin
+        write
+        yield
+      ensure
+        read
+      end
+    end
 
   def write_and_close
     begin
@@ -139,4 +146,5 @@ module ScoutCabinet
   end
 
   alias load_stream importtsv
+  end
 end
