181dbed658dea1ab5d6dc32922f634db1e7cf986
Miguel Vazquez <mikisvaz@gmail.com>
2025-03-16 20:19:53 +0100

    Add support for EntityWorkflows

---
+++ lib/scout/workflow/entity.rb
@@ -0,0 +1,79 @@
+require 'scout/entity'
+require 'scout/workflow'
+
+module EntityWorkflow
+
+  def self.extended(base)
+    base.extend Workflow
+    base.extend Entity
+
+    base.instance_variable_set(:@annotation_inputs, IndiferentHash.setup({})) unless base.instance_variables.include?(:@annotation_inputs)
+    class << base
+      def annotation_input(name, type=nil, desc=nil, default=nil, options = {})
+        annotation name
+        annotation_inputs = self.instance_variable_get("@annotation_inputs")
+        annotation_inputs[name] = [type, desc, default, options]
+      end
+    end
+
+    base.helper :entity do
+      base.setup(clean_name.dup, inputs.to_hash)
+    end
+
+    base.helper :entity_list do 
+      list = inputs.last
+      list = list.load if Step === list
+      base.setup(list, inputs.to_hash)
+    end
+
+    base.property job: :both  do |task_name,options={}|
+      if Array === self && AnnotatedArray === self
+        base.job(task_name, "Default", options.merge(list: self))
+      else
+        base.job(task_name, self, options)
+      end
+    end
+  end
+
+  def property_job(task_name, property_type=:single, *args, &block)
+    task_name, result_type = task_name.keys.first, task_name.values.first if Hash === task_name
+
+    annotation_inputs = self.instance_variable_get("@annotation_inputs")
+    self.annotations.each do |annotation|
+      if annotation_inputs[annotation]
+        input annotation, *annotation_inputs[annotation]
+      else
+        input annotation
+      end
+    end
+    case property_type
+    when :single, :single2array
+      input :entity, :string, "#{self.to_s} identifier", nil, jobname: true
+      task(task_name => result_type, &block)
+    when :both
+      input :entity, :string, "#{self.to_s} identifier", nil, jobname: true
+      input :list, :array, "#{self.to_s} identifier list"
+      task(task_name => result_type, &block)
+    else
+      input :list, :array, "#{self.to_s} identifier list"
+      task(task_name => result_type, &block)
+    end
+
+    property task_name => property_type do |*args|
+      job = job(task_name, *args)
+      Array === job ? job.collect(&:run) : job.run
+    end
+  end
+
+  def entity_job(task_name, *args, &block)
+    property_job(task_name, :single, *args, &block)
+  end
+
+  def list_job(task_name, *args, &block)
+    property_job(task_name, :array, *args, &block)
+  end
+
+  def multiple_job(task_name, *args, &block)
+    property_job(task_name, :multiple, *args, &block)
+  end
+end
---
+++ test/scout/workflow/test_entity.rb
@@ -0,0 +1,58 @@
+require File.expand_path(__FILE__).sub(%r(/test/.*), '/test/test_helper.rb')
+require File.expand_path(__FILE__).sub(%r(.*/test/), '').sub(/test_(.*)\.rb/,'\1')
+
+class TestWorkflowEntity < Test::Unit::TestCase
+  def get_EWF
+    @m ||= Module.new do
+      extend EntityWorkflow
+
+      self.name = 'TestEWF'
+
+      property :introduction do
+        "Mi name is #{self}"
+      end
+
+      entity_job hi: :string do
+        "Hi. #{entity.introduction}"
+      end
+
+      list_job group_hi: :string do
+        "Here is the group: " + entity_list.hi * "; "
+      end
+
+      list_job bye: :array do
+        entity_list.collect do |e|
+          "Bye from #{e}"
+        end
+      end
+    end
+  end
+
+  def test_property_job
+    ewf = get_EWF
+
+    e = ewf.setup("Miki")
+
+    assert_equal "Mi name is Miki", e.introduction
+    assert_equal "Hi. Mi name is Miki", e.hi
+  end
+
+  def test_list
+    ewf = get_EWF
+    
+    l = ewf.setup(["Miki", "Clei"])
+
+    assert_equal 2, l.hi.length
+
+    assert_include l.group_hi, "group: "
+  end
+
+  def test_multiple
+    ewf = get_EWF
+    
+    l = ewf.setup(["Miki", "Clei"])
+
+    assert_equal 2, l.bye.length
+  end
+end
+
