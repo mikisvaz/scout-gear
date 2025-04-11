3a9193c1cfaaa9d59620a24c11a06d8c4e78bcf2
Miguel Vazquez <mikisvaz@gmail.com>
2025-04-11 16:32:11 +0200

    Not rbbt-util module

--- lib/rbbt-scout.rb
+++
@@ -1,8 +0,0 @@
-$LOAD_PATH.unshift File.join(__dir__, '../modules/rbbt-util/lib')
-module Scout
-  extend Resource
-  self.path_maps = Path.path_maps.merge(:rbbt_lib => File.expand_path(File.join(__dir__, '../modules/rbbt-util/', '{TOPLEVEL}','{SUBPATH}')))
-end
-Rbbt = Scout
-
-Resource.set_software_env Rbbt.software
