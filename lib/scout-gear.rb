abb3075e8548af7f482426cc205c31db7a0e345d
Miguel Vazquez <mikisvaz@gmail.com>
2025-05-19 18:56:22 +0200

    Change name of lib path_map

--- lib/scout-gear.rb
+++ lib/scout-gear.rb
@@ -1,7 +1,7 @@
 require 'scout-essentials'
 require_relative 'scout/tsv'
 
-Path.path_maps[:scout_gear] = File.join(Path.caller_lib_dir(__FILE__), "{TOPLEVEL}/{SUBPATH}")
+Path.path_maps[:scout_gear_lib] = File.join(Path.caller_lib_dir(__FILE__), "{TOPLEVEL}/{SUBPATH}")
 
 Persist.cache_dir     = Scout.var.cache.persistence
 TmpFile.tmpdir        = Scout.tmp.find :user
