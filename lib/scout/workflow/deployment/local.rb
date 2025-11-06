051683f930d95c1f3a099f8cd92baa01ba45d17a
Miguel Vazquez <mikisvaz@gmail.com>
2025-11-06 10:10:56 +0100

    Add local executor

---
+++ lib/scout/workflow/deployment/local.rb
@@ -0,0 +1,256 @@
+require_relative 'orchestrator/batches'
+class Workflow::LocalExecutor
+  class NoWork < Exception; end
+
+  def self.process(*args)
+    self.new.process(*args)
+  end
+
+  def self.purge_duplicates(batches)
+    seen = Set.new
+    batches.select do |batch|
+      path = batch[:top_level].path
+      if seen.include? path
+        false
+      else
+        seen << path
+        true
+      end
+    end
+  end
+
+  def self.sort_candidates(batches)
+    seen = Set.new
+    batches.sort_by do |batch|
+      - batch[:resources].values.compact.inject(0){|acc,e| acc += e}
+    end
+  end
+
+  def self.candidates(batches, rules)
+
+    leaf_nodes = batches.select{|b| b[:deps].empty? }
+
+    leaf_nodes.reject!{|b| Workflow::Orchestrator.done_batch?(b) }
+
+    leaf_nodes = purge_duplicates leaf_nodes
+    leaf_nodes = sort_candidates leaf_nodes
+
+    leaf_nodes
+  end
+
+  attr_accessor :available_resources, :resources_requested, :resources_used, :timer
+
+  def initialize(timer = 5, available_resources = nil)
+    available_resources  = {:cpus => Etc.nprocessors } if available_resources.nil?
+    @timer               = timer
+    @available_resources = IndiferentHash.setup(available_resources)
+    @resources_requested = IndiferentHash.setup({})
+    @resources_used      = IndiferentHash.setup({})
+  end
+
+  def release_resources(job)
+    if resources_used[job]
+      Log.debug "Orchestrator releasing resouces from #{job.path}"
+      resources_used[job].each do |resource,value| 
+        next if resource == 'size'
+        resources_requested[resource] -= value.to_i
+      end
+      resources_used.delete job
+    end
+  end
+
+  def check_resources(rules, batch)
+    resources = batch[:resources]
+    job = batch[:top_level]
+
+    limit_resources = resources.select do |resource,value| 
+      value && available_resources[resource] && ((resources_requested[resource] || 0) + value) > available_resources[resource]  
+    end.collect do |resource,v| 
+      resource 
+    end
+
+    if limit_resources.any?
+      Log.debug "Orchestrator waiting on #{job.path} due to #{limit_resources * ", "}"
+    else
+
+      resources_used[job] = resources
+      resources.each do |resource,value| 
+        resources_requested[resource] ||= 0
+        resources_requested[resource] += value.to_i
+      end
+      Log.low "Orchestrator producing #{job.path} with resources #{resources}"
+
+      return yield
+    end
+  end
+
+  def run_batch(batch)
+    job, job_rules = batch.values_at :top_level, :rules
+
+    Scout::Config.with_config do 
+      job_rules[:config_keys].each do |config|
+        Scout::Config.process_config config
+      end if job_rules && job_rules[:config_keys]
+
+      log = job_rules[:log] if job_rules 
+      log = Log.severity if log.nil?
+      Log.with_severity log do
+        job.fork
+      end
+    end
+  end
+
+  def erase_job_dependencies(job, batches)
+    all_jobs = batches.collect{|b| b[:jobs] }.flatten
+    top_level_jobs = batches.collect{|b| b[:top_level] }
+
+    job.dependencies.each do |dep|
+      batch =  batches.select{|b| b[:jobs].include? dep}.first
+      next unless batch
+      rules = batch[:rules]
+      next unless rules[:erase].to_s == 'true'
+
+      dep_path = dep.path
+      parents = all_jobs.select do |parent| 
+        parent.rec_dependencies.include?(dep)
+      end
+
+      next if parents.select{|parent| ! parent.done? }.any?
+
+      parents.each do |parent|
+        Log.high "Erasing #{dep.path} from #{parent.path}"
+        parent.archive_deps
+        parent.copy_linked_files_dir
+        parent.dependencies = parent.dependencies - [dep]
+      end
+
+      dep.clean
+    end
+  end
+
+  def clear_batch(batches, batch)
+    job = batch[:top_level]
+
+    parents = batches.select do |b|
+      b[:deps].include? batch
+    end
+
+    parents.each{|b| b[:deps].delete batch }
+
+    erase_job_dependencies(job, batches)
+  end
+
+  def process(rules, jobs = nil)
+    jobs, rules = rules, {} if jobs.nil?
+    jobs = [jobs] if Step === jobs
+    failed_jobs = []
+    begin
+
+      batches = Workflow::Orchestrator.job_batches(rules, jobs)
+      batches.each do |batch|
+        rules = IndiferentHash.setup batch[:rules]
+        rules.delete :erase if jobs.include?(batch[:top_level])
+        resources = Workflow::Orchestrator.normalize_resources_from_rules(rules)
+        resources = IndiferentHash.add_defaults resources, rules[:default_resources] if rules[:default_resources]
+        batch[:resources] = resources
+        batch[:rules] = rules
+      end
+
+      while batches.reject{|b| Workflow::Orchestrator.done_batch?(b) }.any? 
+
+        candidates = Workflow::LocalExecutor.candidates(batches, rules)
+        top_level_jobs = candidates.collect{|batch| batch[:top_level] }
+
+        raise NoWork, "No candidates and no running jobs #{Log.fingerprint batches}" if resources_used.empty? && top_level_jobs.empty?
+
+        candidates.each do |batch|
+
+          job = batch[:top_level]
+
+          case 
+          when (job.error? || job.aborted?)
+            begin
+              if job.recoverable_error? 
+                if failed_jobs.include?(job)
+                  Log.warn "Failed twice #{job.path} with recoverable error"
+                  next
+                else
+                  failed_jobs << job
+                  job.clean
+                  raise TryAgain
+                end
+              else
+                Log.warn "Non-recoverable error in #{job.path}"
+                next
+              end
+            ensure
+              Log.warn "Releases resources from failed job: #{job.path}"
+              release_resources(job)
+            end
+          when job.done?
+            Log.debug "Orchestrator done #{job.path}"
+            release_resources(job)
+            clear_batch(batches, batch)
+          when job.running?
+            next
+
+          else
+            check_resources(rules, batch) do
+              run_batch(batch)
+            end
+          end
+        end
+
+        batches.each do |batch|
+          job = batch[:top_level]
+          if job.done? || job.aborted? || job.error?
+            clear_batch(batches, batch)
+            release_resources(job)
+            true
+          else
+            false
+          end
+        end
+
+        sleep timer
+      end
+
+      batches.each{|batch| 
+        job = batch[:top_level]
+        begin
+          job.join
+        rescue
+          Log.warn "Job #{job.short_path} ended with exception #{$!.class.to_s}: #{$!.message}"
+        end
+      }
+    rescue TryAgain
+      retry
+    end
+  end
+
+  def self.produce_dependencies(jobs, tasks, produce_cpus = Etc.nprocessors, produce_timer = 5)
+    jobs = [jobs] unless Array === jobs
+    produce_list = []
+    jobs.each do |job|
+      next if job.done? || job.running?
+      job.rec_dependencies.each do |job|
+        produce_list << job if tasks.include?(job.task_name) ||
+          tasks.include?(job.task_name.to_s) ||
+          tasks.include?(job.full_task_name)
+      end
+    end
+
+    orchestrator = self.new produce_timer, cpus: produce_cpus.to_i
+    orchestrator.process({}, produce_list)
+    produce_list
+  end
+
+  def self.produce(jobs, produce_cpus: Etc.nprocessors, produce_timer: 1)
+    jobs = [jobs] unless Array === jobs
+    orchestrator = self.new produce_timer.to_i, cpus: produce_cpus.to_i
+    begin
+      orchestrator.process({}, jobs)
+    rescue self::NoWork
+    end
+  end
+end
