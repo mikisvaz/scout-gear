782acc3c9642dc57c90cfc82e427dac7a773e8dc
Miguel Vazquez <mikisvaz@gmail.com>
2025-11-06 12:10:57 +0100

    Replaced new execution types, local and scheduler

--- lib/scout/workflow/deployment.rb
+++ lib/scout/workflow/deployment.rb
@@ -1,2 +1,4 @@
-require_relative 'deployment/orchestrator'
+require_relative 'deployment/local'
+require_relative 'deployment/scheduler'
 require_relative 'deployment/trace'
+require_relative 'deployment/queue'
