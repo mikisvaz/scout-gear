df07b4b4f54dd2dde37377e3135b4d9331ef7746
Miguel Vazquez <mikisvaz@gmail.com>
2025-11-27 12:20:30 +0100

    Purge info before saving

--- lib/scout/workflow/step/info.rb
+++ lib/scout/workflow/step/info.rb
@@ -30,7 +30,7 @@ class Step
   end
 
   def save_info(info = nil)
-    Persist.save(@info = info, info_file, SERIALIZER)
+    Persist.save(Annotation.purge(@info = info), info_file, SERIALIZER)
     @info_load_time = Time.now
   end
 
