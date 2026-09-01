1e3065350a7c0ac0cc6913103869f7d8ea88bba6
Miguel Vazquez <mikisvaz@gmail.com>
2026-09-01 20:04:25 +0200

    Improve detection of autoinstall workflows

--- lib/scout/workflow.rb
+++ lib/scout/workflow.rb
@@ -53,7 +53,8 @@ module Workflow
     end
 
     def autoinstall
-      @autoinstall || ENV["SCOUT_WORKFLOW_AUTOINSTALL"] = "true"
+      @autoinstall ||= Scout::Config.get(:autoinstall, :workflow, :scout_workflow, :scout, env:"SCOUT_WORKFLOW_AUTOINSTALL", default: 'true').to_s == 'true' if @autoinstall.nil?
+      @autoinstall
     end
 
   end
